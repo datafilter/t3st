00dda1e9edc06194d155a1854e8e350afaba64b8
unknown <46693400+devmachiine@users.noreply.github.com>
2019-02-18 21:24:08 +0100

    Add test and test tests test and test git testing test

---
+++ test.js
@@ -0,0 +1,50 @@
+const assert = (assumption) => {
+    if (!eval(assumption))
+        throw `Evaluation [${assumption}]`
+}
+
+const test = (description, func) => {
+    try {
+        func()
+        return { description: description }
+    } catch (err) {
+        return { description: description, error: err }
+    }
+}
+
+const display_message = test => {
+    let prefix = test.error ? 'error' : 'ok'
+    let postfix = test.error ? ' --> ' + test.error : ''
+    return `[${prefix}] ${test.description}${postfix}`
+}
+
+let ok_test = test(`show [ok] on ok`, () => {
+    let two = 2
+    assert(`${two} == 2`)
+})
+let err_eval = test(`show evaluation on error`, () => {
+    assert(`1 > 2`)
+})
+let err_eval_err = test(`show evaluation exception`, () => {
+    assert(undefined_variable)
+})
+let err_throw = test(`show thrown error`, () => {
+    throw 'ThrownError'
+})
+let ok_test_tests = test(`test functions yield expected results with correct messages`, () => {
+    let test_test = (result, expected_assert, expected_message) => {
+        let passed = !result.error
+        let message = display_message(result)
+        assert(`${passed === expected_assert} && '${result.description}'`)
+        assert(`'${message}' === '${expected_message}'`)
+    }
+    test_test(ok_test, true, '[ok] show [ok] on ok')
+    test_test( err_eval, false, '[error] show evaluation on error --> Evaluation [1 > 2]')
+    test_test(err_eval_err, false, '[error] show evaluation exception --> ReferenceError: undefined_variable is not defined')
+    test_test(err_throw, false, '[error] show thrown error --> ThrownError')
+})
+
+const print = console.log
+let display = (test) => print(display_message(test))
+display(ok_test_tests)
+print('-'.repeat(99))
