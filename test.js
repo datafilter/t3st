727a375799dd3e6ebe5953efb4d7c69d9b3c0f90
unknown <46693400+devmachiine@users.noreply.github.com>
2019-03-09 18:16:07 +0100

    Test test results

--- test.js
+++ test.js
@@ -3,9 +3,10 @@
     const framework = require('./index')
     const show = console.log
 
+    const test_results = require('./tests/test_results')(framework)
     const tally_results_tests = require('./tests/tally_results')(framework)
 
-    const framework_tests = tally_results_tests
+    const framework_tests = [test_results, tally_results_tests]
 
     show('-'.repeat(40))
     show(framework.tally_results('framework', ...framework_tests))
