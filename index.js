de0b63500046afbbe4c521f10d0ae1909e3abfa4
unknown <46693400+devmachiine@users.noreply.github.com>
2019-03-14 22:42:08 +0100

    Replace require(..) each test file with test(directory)

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 const { test, assert, affirm } = require('./lib/t3st')
 const { result_text, tally_results } = require('./lib/text')
 
+const framework = { test, assert, affirm, result_text, tally_results }
+
+const io = require('./lib/io')
+
+const test_file = (file_path) => {
+    try {
+        return require(file_path)(framework)
+    } catch (err) {
+        return {
+            description: `Error in file : ${file_path}`,
+            error:
+                `[err] exception occurred outside of tests\n\t--> ${err}`
+        }
+    }
+}
+
+const test_output = async (dir, label = '', func_or_file = console.log) => {
+    const test_results = await Promise.all(
+        io.walk_dir(dir)
+            .map(test_file))
+    const summary = tally_results(label, test_results)
+    switch (typeof func_or_file) {
+        case 'function':
+            return func_or_file(summary)
+        case 'string':
+            return io.save_data(func_or_file, summary)
+    }
+    throw 'Unexpected function or file in test_dir(function || filepath)'
+}
+
 module.exports = {
     test
     , assert
     , affirm
     , result_text
     , tally_results
+    , test_output
 }
--- test.js
+++ test.js
@@ -1,27 +1,14 @@
 (async () => {
 
-    const framework = require('./index')
-    const show = console.log
+  const framework = require('./index')
 
-    const framework_tests = [
-        require('./tests/test_results')(framework)
-      ,  require('./tests/test_test')(framework)
-      ,  require('./tests/test_bool')(framework)
-      ,  require('./tests/test_now')(framework)
-      ,  await require('./tests/test_async')(framework)
-      ,  require('./tests/assert')(framework)
-      ,  require('./tests/affirm')(framework)
-      ,  require('./tests/result_text')(framework)
-      ,  require('./tests/tally_results')(framework)
-    ]
-    
-    show('-'.repeat(40))
-    show(framework.tally_results('framework', ...framework_tests))
+  console.log('-'.repeat(40))
+
+  framework.test_output('./tests')
 
 })()
 
 // todo's
-// function that points to folder and runs all tests found.
 // Within test itself, and all  assert variants? -> Test failed *before* assertion
 // Similar naming convention, wrt:
 //    'strings' or "strings"
