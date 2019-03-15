9b47ef70242ce768755d6b1ebf815d95fd106a15
unknown <46693400+devmachiine@users.noreply.github.com>
2019-03-15 20:08:04 +0100

    Move basic ok empty test

--- tests/test_now.js
+++ tests/test_now.js
@@ -3,7 +3,7 @@ module.exports = (framework) => {
     const { test, assert, affirm } = framework
 
     const now_tests = [
-        
+        test("OK test passed with empty body", () => { })
     ]
 
     return now_tests
--- tests/test_results.js
+++ tests/test_results.js
@@ -3,8 +3,7 @@ module.exports = (framework) => {
     const { test, assert, affirm } = framework
 
     const test_results = [
-        test("OK test passed with empty body", () => { })
-        , test("OK test returns it's own description, without error", () => {
+        test("OK test returns it's own description, without error", () => {
             const ok_result = test("my description", () => { })
             affirm(() => ok_result && !!ok_result.description)
             affirm(() => !ok_result.error)
@@ -20,7 +19,7 @@ module.exports = (framework) => {
         , test("Error stack is included in error if available", () => {
             const err_result = test("", () => { throw new Error('#20?') })
             affirm(() => typeof err_result.error.stack !== undefined)
-            affirm(err_result.error, () => err_result.error.stack.includes('test_results.js:21'))
+            affirm(err_result.error, () => err_result.error.stack.includes('test_results.js:20'))
         })
     ]
 
