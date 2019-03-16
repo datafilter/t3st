63ec42bde66fc30224b86bb0dae6011400134080
unknown <46693400+devmachiine@users.noreply.github.com>
2019-03-16 14:36:59 +0100

    Move require_tests into t3st-lib

--- index.js
+++ index.js
@@ -5,31 +5,9 @@ const framework = { test, assert, affirm, result_text, tally_results }
 
 const io = require('./t3st-lib/io')
 
-const test_file = (file_path) => {
-    try {
-        return require(file_path)(framework)
-    } catch (err) {
-        return {
-            description: `Error in file : ${file_path}`,
-            error: `[err] exception occurred outside of tests\n\t--> ${err}`,
-            trace: ` ${err.stack ? err.stack : 'unknown source'}`
-        }
-    }
-}
-
-const require_tests = async (dir, label = '', func = console.log) => {
-    const test_results = await Promise.all(
-        io.walk_dir(dir)
-            .map(test_file))
-    const summary = tally_results(label, test_results)
-    func(summary)
-}
+const require_tests = require('./t3st-lib/require-tests')(io, framework)
 
 module.exports = {
-    test
-    , assert
-    , affirm
-    , result_text
-    , tally_results
+    ...framework
     , require_tests
 }
---
+++ t3st-lib/require-tests.js
@@ -0,0 +1,20 @@
+module.exports = (io, framework) => async (dir, label = '', func = console.log) => {
+
+    const test_file = (file_path) => {
+        try {
+            return require(file_path)(framework)
+        } catch (err) {
+            return {
+                description: `Error in file : ${file_path}`,
+                error: `[err] exception occurred outside of tests\n\t--> ${err}`,
+                trace: ` ${err.stack ? err.stack : 'unknown source'}`
+            }
+        }
+    }
+
+    const test_results = await Promise.all(io.walk_dir(dir).map(test_file))
+
+    const summary = framework.tally_results(label, test_results)
+
+    func(summary)
+}
