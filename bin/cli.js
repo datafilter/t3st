fa50d86766d5f25571db21232f260b18c8dc85b0
unknown <46693400+devmachiine@users.noreply.github.com>
2019-09-23 20:35:09 +0200

    Add cli

---
+++ bin/cli.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+console.log('hi cli!')
+
+// const [_path,_file,...args] = process.argv
+
+// (async () => {
+//     const { run } = require('./index')
+//     console.log('-'.repeat(40))
+//     await run('./tests'})
+//   })()
