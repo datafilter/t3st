ce983b458756b38e4e9b90cabc21ba7d85bb53f8
unknown <46693400+devmachiine@users.noreply.github.com>
2019-09-27 22:07:18 +0200

    call run(cwd [,target_dir]) from cli

--- bin/cli.js
+++ bin/cli.js
@@ -1,11 +1,29 @@
 #!/usr/bin/env node
+(async () => {
 
-console.log('hi cli!')
+    const [_node_exec_path, _cli_file_path, target_dir, ...args] = process.argv
+    //todo test each dir passed in args
 
-// const [_path,_file,...args] = process.argv
+    const entry_dir = process.cwd()
+    const test_dir = target_dir
 
-// (async () => {
-//     const { run } = require('./index')
-//     console.log('-'.repeat(40))
-//     await run('./tests'})
-//   })()
+    const { run } = require('../index.js')
+    console.log('-'.repeat(40))
+    await run({ entrypoint_dir: entry_dir, dir: test_dir })
+})()
+
+// IN PROJECT DIR
+// t3st
+// --> run(root/tests) [OK]
+// t3st folder
+// --> run(root/folder) [OK]
+
+// OUTSIDE PROJECT DIR
+// t3st
+// --> no tests found.
+// t3st folder
+// --> run (cwd/folder/tests)
+// t3st folder/tests
+// --> run (cwd/folder/tests) [OK]
+
+//TODO Walk.dir no dir found.
