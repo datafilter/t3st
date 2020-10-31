89a4b42c870e5eb0500d773d5018787cccc85eb9
devmachiine <46693400+devmachiine@users.noreply.github.com>
2020-10-31 19:16:22 +0000

    test undefined test results are ignored

--- tests-external/all_ok/all_ok.js
+++ tests-external/all_ok/all_ok.js
@@ -1,6 +1,9 @@
+/* eslint-disable no-sparse-arrays */
 module.exports = ({ test, equal, check }) => [
     test("ok", () => true),
     test("also ok", () => { equal(1, 1) }),
     test("also ok", () => { check(1, (uno) => uno === 1) }),
+    /* additional undefined and undefined sub values */
+    , , , [, , [, ,]], ,
     test("also ok", () => { equal({ a: 1, b: 2 }, { b: 2, a: 1 }) })
 ]
