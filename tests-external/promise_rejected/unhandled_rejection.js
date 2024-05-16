e23336a4cabb54174bb31e7d1bb3a0883c49bbd4
devmachiine <46693400+devmachiine@users.noreply.github.com>
2024-05-16 23:20:18 +0200

    remove process.unhandledRejection listener

--- tests-external/promise_rejected/unhandled_rejection.js
+++ tests-external/promise_rejected/unhandled_rejection.js
@@ -1,6 +1,5 @@
 module.exports = async ({ test }) => [
     await test("unhandled promise rejection", async () => {
-        // node --unhandled-rejections=strict
         const _r = Promise.reject('unobserved')
     })
 ]
